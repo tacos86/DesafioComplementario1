cc19c6b90cedfc4b46670807ca60e4cfe9aea441
bcosta <49333564j@gmail.com>
2021-07-01 22:23:19 -0300

    function

--- archivo.js
+++ archivo.js
@@ -7,3 +7,10 @@ const precioFinal = iva * precio;
 
 
 document.write ("El importe final es $ " + precioFinal);
+
+/*const precioFinal = 
+  function calcular (iva, precio) {
+    return iva * precio;    
+}
+
+document.write("El Importe total es $ " + precioFinal)*/
